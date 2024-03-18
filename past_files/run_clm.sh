ee7d81273caa5c6afa179b0e98b87cdf87552d02
xuekaizhu_linlab <xuekaizhu0@gmail.com>
2024-03-18 22:57:14 +0800

    updata

--- past_files/run_clm.sh
+++
@@ -1,9 +0,0 @@
-python run_clm.py \
-    --model_name_or_path pre_trained_model/facebook/opt-350m \
-    --dataset_name wikitext \
-    --dataset_config_name wikitext-2-raw-v1 \
-    --per_device_train_batch_size 1 \
-    --per_device_eval_batch_size 1 \
-    --do_train \
-    --do_eval \
-    --output_dir /tmp/test-clm
--- past_files/run_inference.sh
+++
@@ -1,7 +0,0 @@
-deepspeed --num_nodes 1 --num_gpus 1 inference-test.py \
-    --name model/code-t5 \
-    --batch_size 32 \
-    --local_dataset_path dataset/gsm8k-0306/test_file/refine-test_add_code.json \
-    --output_dir model/code-t5 \
-    # --greedy 
-    # --add_solution 
