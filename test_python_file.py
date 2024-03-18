49ae71de41328ea7e2e35e77f878474e1e1df72c
xuekaizhu_linlab <xuekaizhu0@gmail.com>
2024-03-18 22:04:29 +0800

    updata code

--- test_python_file.py
+++
@@ -1,8 +0,0 @@
-def solution():
-    movie_c_length = 1.25 * 60
-    movie_b_length = movie_c_length + 5
-    movie_a_length = movie_b_length / 4
-    result = movie_a_length
-    return result
-
-print(solution())
